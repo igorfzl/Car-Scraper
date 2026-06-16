a18207c49ec550567a6e5f5100a57dd245b83cf8
igorfzl <igorworldfazoli@gmail.com>
2026-06-16 10:21:31 -0300

    Ajustado mudança de lógicas do Main para o Service

--- src/main/java/br/edu/utfpr/td/tsi/raspador/App.java
+++ src/main/java/br/edu/utfpr/td/tsi/raspador/App.java
@@ -1,27 +1,17 @@
 package br.edu.utfpr.td.tsi.raspador;
 
-import br.edu.utfpr.td.tsi.raspador.etl.Carregador;
-import br.edu.utfpr.td.tsi.raspador.etl.Extrator;
-import br.edu.utfpr.td.tsi.raspador.etl.Job;
-import br.edu.utfpr.td.tsi.raspador.etl.ArquivoJsonCarregador;
-import br.edu.utfpr.td.tsi.raspador.modelo.Veiculo;
-import br.edu.utfpr.td.tsi.raspador.transformadores.ChavesNaMaoTransformador;
-import br.edu.utfpr.td.tsi.raspador.transformadores.LocalizaTransformador;
-import br.edu.utfpr.td.tsi.raspador.transformadores.NaPistaTransformador;
+import br.edu.utfpr.td.tsi.raspador.servico.OrquestradorService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.jsoup.nodes.Document;
-
-import java.util.List;
 
 @SpringBootApplication
 public class App implements CommandLineRunner {
 
-    private final Extrator<Document> extratorPadrao;
+    private final OrquestradorService orquestrador;
 
-    public App(Extrator<Document> extratorPadrao) {
-        this.extratorPadrao = extratorPadrao;
+    public App(OrquestradorService orquestrador) {
+        this.orquestrador = orquestrador;
     }
 
     public static void main(String[] args) {
@@ -30,59 +20,6 @@ public class App implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        int limitePaginas = 15; // O limite que você definiu
-
-        extrairNaPista(limitePaginas);
-        extrairChavesNaMao(limitePaginas);
-        extrairLocaliza(limitePaginas);
-
-        System.out.println("\nExtração em lote finalizada com sucesso!");
-    }
-
-    private void extrairChavesNaMao(int limite) {
-        System.out.println("\n=== INICIANDO RASPAGEM: CHAVES NA MÃO ===");
-
-        ArquivoJsonCarregador carregador = new ArquivoJsonCarregador("veiculos_chavesnamao.json");
-        Job<Document, List<Veiculo>> job = new Job<>(this.extratorPadrao, new ChavesNaMaoTransformador(), carregador);
-
-        for (int i = 1; i <= limite; i++) {
-            System.out.println("-> Extraindo página " + i + "...");
-            job.executar("https://www.chavesnamao.com.br/carros-usados/pr-cascavel/?pg=" + i);
-            pausa(1500); // Pausa anti-ban
-        }
-    }
-
-    private void extrairNaPista(int limite) {
-        System.out.println("\n=== INICIANDO RASPAGEM: NA PISTA ===");
-
-        ArquivoJsonCarregador carregador = new ArquivoJsonCarregador("veiculos_napista.json");
-        Job<Document, List<Veiculo>> job = new Job<>(this.extratorPadrao, new NaPistaTransformador(), carregador);
-
-        for (int i = 1; i <= limite; i++) {
-            System.out.println("-> Extraindo página " + i + "...");
-            job.executar("https://napista.com.br/busca/carro/cascavel?pn=" + i);
-            pausa(3000);
-        }
-    }
-
-    private void extrairLocaliza(int limite) {
-        System.out.println("\n=== INICIANDO RASPAGEM: LOCALIZA SEMINOVOS ===");
-
-        ArquivoJsonCarregador carregador = new ArquivoJsonCarregador("veiculos_localiza.json");
-        Job<Document, List<Veiculo>> job = new Job<>(this.extratorPadrao, new LocalizaTransformador(), carregador);
-
-        for (int i = 1; i <= limite; i++) {
-            System.out.println("-> Extraindo página " + i + "...");
-            job.executar("https://seminovos.localiza.com/carros/pr-cascavel?page=" + i);
-            pausa(2000);
-        }
-    }
-
-    private void pausa(int milissegundos) {
-        try {
-            Thread.sleep(milissegundos);
-        } catch (InterruptedException e) {
-            System.err.println("Erro na pausa do sistema.");
-        }
+        orquestrador.iniciarRaspagem(20);
     }
 }
---
+++ src/main/java/br/edu/utfpr/td/tsi/raspador/servico/OrquestradorService.java
@@ -0,0 +1,75 @@
+package br.edu.utfpr.td.tsi.raspador.servico;
+
+import br.edu.utfpr.td.tsi.raspador.etl.Extrator;
+import br.edu.utfpr.td.tsi.raspador.etl.Job;
+import br.edu.utfpr.td.tsi.raspador.etl.ArquivoJsonCarregador;
+import br.edu.utfpr.td.tsi.raspador.modelo.Veiculo;
+import br.edu.utfpr.td.tsi.raspador.transformadores.ChavesNaMaoTransformador;
+import br.edu.utfpr.td.tsi.raspador.transformadores.LocalizaTransformador;
+import br.edu.utfpr.td.tsi.raspador.transformadores.NaPistaTransformador;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service // <-- Define que é a camada de negócio/lógica
+public class OrquestradorService {
+
+    private final Extrator<Document> extratorPadrao;
+
+    // O Spring injeta o extrator aqui automaticamente
+    public OrquestradorService(Extrator<Document> extratorPadrao) {
+        this.extratorPadrao = extratorPadrao;
+    }
+
+    public void iniciarRaspagem(int limitePaginas) {
+        extrairChavesNaMao(limitePaginas);
+        extrairNaPista(limitePaginas);
+        extrairLocaliza(limitePaginas);
+        System.out.println("\nExtração em lote finalizada com sucesso!");
+    }
+
+    private void extrairChavesNaMao(int limite) {
+        System.out.println("\n=== INICIANDO RASPAGEM: CHAVES NA MÃO ===");
+        ArquivoJsonCarregador carregador = new ArquivoJsonCarregador("veiculos_chavesnamao.json");
+        Job<Document, List<Veiculo>> job = new Job<>(this.extratorPadrao, new ChavesNaMaoTransformador(), carregador);
+
+        for (int i = 1; i <= limite; i++) {
+            System.out.println("-> Extraindo página " + i + "...");
+            job.executar("https://www.chavesnamao.com.br/carros-usados/pr-cascavel/?pg=" + i);
+            pausa(1500);
+        }
+    }
+
+    private void extrairNaPista(int limite) {
+        System.out.println("\n=== INICIANDO RASPAGEM: NA PISTA ===");
+        ArquivoJsonCarregador carregador = new ArquivoJsonCarregador("veiculos_napista.json");
+        Job<Document, List<Veiculo>> job = new Job<>(this.extratorPadrao, new NaPistaTransformador(), carregador);
+
+        for (int i = 1; i <= limite; i++) {
+            System.out.println("-> Extraindo página " + i + "...");
+            job.executar("https://napista.com.br/busca/carro/cascavel?pn=" + i);
+            pausa(2000);
+        }
+    }
+
+    private void extrairLocaliza(int limite) {
+        System.out.println("\n=== INICIANDO RASPAGEM: LOCALIZA SEMINOVOS ===");
+        ArquivoJsonCarregador carregador = new ArquivoJsonCarregador("veiculos_localiza.json");
+        Job<Document, List<Veiculo>> job = new Job<>(this.extratorPadrao, new LocalizaTransformador(), carregador);
+
+        for (int i = 1; i <= limite; i++) {
+            System.out.println("-> Extraindo página " + i + "...");
+            job.executar("https://seminovos.localiza.com/carros/pr-cascavel?page=" + i);
+            pausa(2000);
+        }
+    }
+
+    private void pausa(int milissegundos) {
+        try {
+            Thread.sleep(milissegundos);
+        } catch (InterruptedException e) {
+            System.err.println("Erro na pausa do sistema.");
+        }
+    }
+}
